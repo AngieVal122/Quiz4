c11fa326cadb349c9489ec12d86ef6ac79be7a66
AngieVal122 <142047070+AngieVal122@users.noreply.github.com>
2023-09-30 23:19:52 -0500

    Update Estudiante.java

--- Estudiante.java
+++ Estudiante.java
@@ -1,12 +1,8 @@
 import java.time.LocalDate;
 import java.time.Period;
-/**
- * clase Estudiante
- */
+
 public class Estudiante {
-    /**
-     * Atributos
-     */
+   
     private String nombre;
     private double notaMateria1;
     private double notaMateria2;
@@ -23,21 +19,7 @@ public class Estudiante {
         this.getPromedio = getPromedio;
         this.fechaNacimiento = fechaNacimiento;
     }
-    /**
-     * Método constuctor para inicializar los atributos de la clase
-     * @param nombre nombre del estudiante
-     * @param fechaNacimiento fecha de nacimiento del estudiante
-     * @param notaMateria1 nota numero uno del estudiante
-     * @param notaMateria2 nota numero dos del estudiante
-     * @param notaMateria3 nota numero tres del estudiante
-     * @param getPromedio promedio de notas del estudiante
-     *
-     */
-
-
-    /**
-     * Getter
-     */
+    
     public void setNotaMateria1(double notaMateria1) {
         this.notaMateria1 = notaMateria1;
     }
@@ -50,9 +32,7 @@ public class Estudiante {
         this.notaMateria3 = notaMateria3;
     }
 
-    /**
-     * Setter
-     */
+   
     public String getNombre() {
         return nombre;
     }
@@ -69,9 +49,7 @@ public class Estudiante {
         return notaMateria3;
     }
 
-    /**
-     * Método edad actual
-     */
+    
     public int getEdad() {
         LocalDate fechaActual = LocalDate.now();
         Period periodo = Period.between(fechaNacimiento, fechaActual);
@@ -82,9 +60,7 @@ public class Estudiante {
         return fechaNacimiento;
     }
 
-    /**
-     * Método promedio de notas
-     */
+    
     public double getPromedio (){
         return (notaMateria1+notaMateria2+notaMateria3)/3;
     }

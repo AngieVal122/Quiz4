2ff9c97439a1da2f2d330f33ce532630922501fc
Valentina Ibagué <angiv.56@gmail.com>
2023-09-30 23:00:07 -0500

    Quiz 4 finalizado.

--- Estudiante.java
+++ Estudiante.java
@@ -1,63 +1,95 @@
-import java.lang.reflect.AnnotatedArrayType;
 import java.time.LocalDate;
-import java.util.Date;
-
-public class Estudiante{
+import java.time.Period;
+/**
+ * clase Estudiante
+ */
+public class Estudiante {
+    /**
+     * Atributos
+     */
     private String nombre;
-    private Date fechaNacimiento;
-    private double materia1;
-    private double materia2;
-    private double materia3;
+    private double notaMateria1;
+    private double notaMateria2;
+    private double notaMateria3;
+    private LocalDate fechaNacimiento;
+    private double getPromedio;
 
-    public Estudiante(String nombre, Date fechaNacimiento) {
+
+    public Estudiante(String nombre, LocalDate fechaNacimiento) {
         this.nombre = nombre;
+        this.notaMateria1 = 0.0;
+        this.notaMateria2 = 0.0;
+        this.notaMateria3 = 0.0;
+        this.getPromedio = getPromedio;
         this.fechaNacimiento = fechaNacimiento;
-        this.materia1 = 4.0;
-        this.materia2 = 3.0;
-        this.materia3 = 4.5;
+    }
+    /**
+     * Método constuctor para inicializar los atributos de la clase
+     * @param nombre nombre del estudiante
+     * @param fechaNacimiento fecha de nacimiento del estudiante
+     * @param notaMateria1 nota numero uno del estudiante
+     * @param notaMateria2 nota numero dos del estudiante
+     * @param notaMateria3 nota numero tres del estudiante
+     * @param getPromedio promedio de notas del estudiante
+     *
+     */
+
+
+    /**
+     * Getter
+     */
+    public void setNotaMateria1(double notaMateria1) {
+        this.notaMateria1 = notaMateria1;
     }
 
-    public int getEdad(){
-        LocalDate hoy = LocalDate.now();
-        int edad= fechaNacimiento - LocalDate hoy;
-        return  edad ;
+    public void setNotaMateria2(double notaMateria2) {
+        this.notaMateria2 = notaMateria2;
     }
 
-    public double getPromedio(){
-        double suma = (materia1+materia2+materia3);
-        double promedio = suma /3;
-return promedio;
+    public void setNotaMateria3(double notaMateria3) {
+        this.notaMateria3 = notaMateria3;
     }
 
+    /**
+     * Setter
+     */
     public String getNombre() {
         return nombre;
     }
 
-    public Date getFechaNacimiento() {
+    public double getNotaMateria1() {
+        return notaMateria1;
+    }
+
+    public double getNotaMateria2() {
+        return notaMateria2;
+    }
+
+    public double getNotaMateria3() {
+        return notaMateria3;
+    }
+
+    /**
+     * Método edad actual
+     */
+    public int getEdad() {
+        LocalDate fechaActual = LocalDate.now();
+        Period periodo = Period.between(fechaNacimiento, fechaActual);
+        return periodo.getYears();
+    }
+
+    public LocalDate getFechaNacimiento() {
         return fechaNacimiento;
     }
 
-    public double getMateria1() {
-        return materia1;
+    /**
+     * Método promedio de notas
+     */
+    public double getPromedio (){
+        return (notaMateria1+notaMateria2+notaMateria3)/3;
     }
 
-    public double getMateria2() {
-        return materia2;
-    }
-
-    public double getMateria3() {
-        return materia3;
-    }
-
-    public void setMateria1(double materia1) {
-        this.materia1 = materia1;
-    }
-
-    public void setMateria2(double materia2) {
-        this.materia2 = materia2;
-    }
-
-    public void setMateria3(double materia3) {
-        this.materia3 = materia3;
+    public double getGetPromedio() {
+        return getPromedio;
     }
 }
--- Principal.java
+++ Principal.java
@@ -1,12 +1,31 @@
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Principal {
     public static void main(String[] args) {
-        Date fechaNacAngie = new Date(2012, 05, 14);
-        Estudiante angie = new Estudiante("Angie", fechaNacAngie);
+        Date day = new Date();
+        System.out.printf("Fecha Actual: %td/%tm/%tY %n", day, day, day);
 
-        System.out.println(angie.getPromedio());
-        System.out.println(angie.getEdad());
+        LocalDate fechaNacimientoAngie = LocalDate.of(2012, 7, 9);
+        Estudiante Angie = new Estudiante("Angie", fechaNacimientoAngie);
+        Angie.setNotaMateria1(3.6);
+        Angie.setNotaMateria2(4.0);
+        Angie.setNotaMateria3(3.8);
 
+        System.out.println("Nombre: " + Angie.getNombre() + "\n" + "Fecha de nacimiento: " + Angie.getFechaNacimiento());
+        System.out.println("Nota 1: " + Angie.getNotaMateria1() + "\n" + "Nota 2: " + Angie.getNotaMateria2() + "\n" + "Nota 3: " + Angie.getNotaMateria3() );
+        System.out.println("Promedio: " + Angie.getPromedio());
+        System.out.println("Tiene " + Angie.getEdad() + " años. \n" );
+
+        LocalDate fechaNacimientoSebastian = LocalDate.of(2008, 8, 25);
+        Estudiante Sebastian = new Estudiante("Sebastian", fechaNacimientoSebastian);
+        Sebastian.setNotaMateria1(4.8);
+        Sebastian.setNotaMateria2(4.2);
+        Sebastian.setNotaMateria3(3.3);
+
+        System.out.println("Nombre: " + Sebastian.getNombre() + "\n" + "Fecha de nacimiento: " + Sebastian.getFechaNacimiento());
+        System.out.println("Nota 1: " + Sebastian.getNotaMateria1() + "\n" + "Nota 2: " + Sebastian.getNotaMateria2() + "\n" + "Nota 3: " + Sebastian.getNotaMateria3() );
+        System.out.println("Promedio: " + Sebastian.getPromedio());
+        System.out.println("Tiene " + Sebastian.getEdad() + " años.");
     }
 }
